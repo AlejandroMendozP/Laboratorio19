7a025c87ff25531c16ad016f6e8e8ae63a5afe28
Alejandro <amendozapan@unsa.edu.pe>
2025-12-01 16:35:43 -0500

    Ejercicio 05

---
+++ ejercicio05.js
@@ -0,0 +1,14 @@
+async function obtenerDatos() {
+  try {
+    const res = await fetch("https://pokeapi.co/api/v2/pokemon/pikachu");
+    const data = await res.json();
+
+    console.log("Altura:", data.height);
+    console.log("Peso:", data.weight);
+
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+obtenerDatos();
